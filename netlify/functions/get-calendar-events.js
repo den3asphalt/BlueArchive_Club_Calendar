9e0814fae4973af5c9b34c7e1f405f1f1165acf8
den3asphalt <86176261+den3asphalt@users.noreply.github.com>
2025-08-13 00:07:21 +0900

    修正2

--- netlify/functions/get-calendar-events.js
+++ netlify/functions/get-calendar-events.js
@@ -13,7 +13,7 @@ exports.handler = async function(event, context) {
         // GraphQLクエリからcategoryを削除
         // 500を越える場合は別途色々する必要あり
         const query = `
-          query AllRecruitmentInfos() {
+          query {
             allRecruitmentInfos(orderBy: startDateTime_ASC, first: 500) {
               id
               club {
